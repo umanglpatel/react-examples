b76e931231be0c4608a1222287bf985917fd2f2d
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-04-06 16:41:13 +0530

    Section4 - lists-conditionals-07-using-immutable-updates

--- src/App.js
+++ src/App.js
@@ -27,7 +27,8 @@ class App extends Component {
   }
 
   deletePerson = (personIndex) => {
-    const persons = this.state.persons;
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({ persons: persons });
   }
