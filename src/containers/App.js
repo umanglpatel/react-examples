41114406bd09d71eab827e76765c3123b569c9ab
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-07-19 16:31:50 +0530

    Section 5.8 - using css classes in app button

--- src/containers/App.js
+++ src/containers/App.js
@@ -41,16 +41,9 @@ class App extends Component {
   }
 
   render() {
-
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px'
-    }
-
     let persons = null;
+    let btnClass = '';
+
     if (this.state.showPersons) {
 
       persons = (
@@ -61,7 +54,7 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
     }
 
     const assignedClasses = [];
@@ -75,7 +68,7 @@ class App extends Component {
       <div className={classes.App} >
         <h1> Hi!I am react app </h1>
         <p className={assignedClasses.join(' ')}> this is really working! </p>
-        <button style={style} onClick={this.togglePersonsHandler} > Toggle Persons </button>
+        <button className={btnClass} onClick={this.togglePersonsHandler} > Toggle Persons </button>
         {/* {this.state.showPersons ? this.renderPerson() : null} */}
         {persons}
       </div>
