d7b597bd68ad964bdec96b2ccb0be7a4b871a731
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-08-03 16:27:42 +0530

    Section 7.17 Passing unknown props in higher order components

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -11,7 +11,7 @@ const withClass = (props) => {
 const usingClass = (WrappedComponent, className) => {
     return (props) => (
         <div className={className}>
-            <WrappedComponent />
+            <WrappedComponent {...props} />
         </div>
     )
 }
